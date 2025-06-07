f367c940d2358e88b618bcd439ce41cb55f89856
Varshit Dusad <dusadvarshit@users.noreply.github.com>
2025-06-07 20:05:14 +0530

    Fixed the kaggle dataset fetch function

--- src/mlutils/utils/kaggle.py
+++ src/mlutils/utils/kaggle.py
@@ -1,13 +1,16 @@
 import kagglehub
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Download latest version
-def fetch_kaggle_dataset(dataset_name: str, path: str = None):
+def fetch_kaggle_dataset(dataset_name: str):
     """
     Fetch a Kaggle dataset by its name.
 
     :param dataset_name: The name of the Kaggle dataset to fetch.
-    :param path: Optional path to save the dataset files.
-    :return: Path to the downloaded dataset files.
+    :path: Path to the downloaded dataset files.
     """
-    return kagglehub.dataset_download(dataset_name, path=path)
+    path = kagglehub.dataset_download(dataset_name)
+    return path
