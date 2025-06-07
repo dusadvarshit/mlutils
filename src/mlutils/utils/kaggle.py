8a123f848250ad33f74161ddc91da31ed5b9fdd2
Varshit Dusad <dusadvarshit@users.noreply.github.com>
2025-06-07 19:04:14 +0530

    Added kaggle dataset fetch function

--- src/mlutils/utils/kaggle.py
+++ src/mlutils/utils/kaggle.py
@@ -1,6 +1,13 @@
 import kagglehub
 
-# Download latest version
-path = kagglehub.dataset_download("blastchar/telco-customer-churn")
 
-print("Path to dataset files:", path)
+# Download latest version
+def fetch_kaggle_dataset(dataset_name: str, path: str = None):
+    """
+    Fetch a Kaggle dataset by its name.
+
+    :param dataset_name: The name of the Kaggle dataset to fetch.
+    :param path: Optional path to save the dataset files.
+    :return: Path to the downloaded dataset files.
+    """
+    return kagglehub.dataset_download(dataset_name, path=path)
